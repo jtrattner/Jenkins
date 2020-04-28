bfd7bcc4259d60ea88cc8ea7bd1a4a6d70d6b290
Johannes Trattner <johannes.trattner@student.tugraz.at>
2020-04-28 15:11:02 +0200

    changed folder

--- job_dsl/src/main/resources/jobs/dockerBaseImage.groovy
+++ job_dsl/src/main/resources/jobs/dockerBaseImage.groovy
@@ -1,6 +1,6 @@
 // The file should not be named jenkins.groovy as that leads to a warning as there
 // is already a jenkins package.
-def releaseJobBuilder = new JobsBuilder(this).folder('generated_job_test', {})
+def releaseJobBuilder = new JobsBuilder(this).folder('lab/Johannes_Trattner/generated_job_test', {})
 def baseImageJobBuilder = releaseJobBuilder.pipeline()
 
 String basePath = 'example-path'
